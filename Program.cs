a04df76a459a40c49aaa81a920d3526ab1106f93
Adriano Teixeira <adriano180@hotmail.com>
2022-08-14 20:27:45 -0300

    Quarta versão

--- Program.cs
+++ Program.cs
@@ -5,6 +5,11 @@
         Console.WriteLine("Hello, World!");
         Console.WriteLine("2 versao de teste");
         Console.WriteLine("test teste teste");
+        Console.Write("teste");
+        int a = 5;
+        double b = 5.5;
+        
+
         int a = Console.Read();
         
         int b = int.Parse(Console.ReadLine());
