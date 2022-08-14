cba21c365744eb84128c86f07bb7cb8b020a8c24
Adriano Teixeira <adriano180@hotmail.com>
2022-08-14 19:28:05 -0300

    Primeira Versão

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello, World!");
+        int a = Console.Read();
+        int b = int.Parse(Console.ReadLine());
+
+
+    }
+}
