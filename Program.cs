689d445150a6a0b278208c15a39737185582e7d5
Adriano Teixeira <adriano180@hotmail.com>
2022-08-14 19:40:27 -0300

    Adicionou comentáios apenas

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@
         Console.WriteLine("Hello, World!");
         Console.WriteLine("Hello, World!");
         int a = Console.Read();
+        // comentario
         int b = int.Parse(Console.ReadLine());
 
 
