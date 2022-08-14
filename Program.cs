d98ff05607466b756ac2ba7608d51973cddb8aae
Adriano Teixeira <adriano180@hotmail.com>
2022-08-14 20:24:49 -0300

    Terceita versão

--- Program.cs
+++ Program.cs
@@ -3,11 +3,12 @@
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("2 versao de teste");
+        Console.WriteLine("test teste teste");
         int a = Console.Read();
-        // comentario
+        
         int b = int.Parse(Console.ReadLine());
-
+        // comentario movido
 
     }
 }
